9e36cbcc00b883b8296ea6289489d887b8555ace
profbruuno <96679152+profbruuno@users.noreply.github.com>
2025-10-26 12:49:14 +0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,30 @@
+<?php
+header('Access-Control-Allow-Origin: *');
+header('Content-Type: application/json');
+header('Access-Control-Allow-Methods: GET, POST, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type');
+
+class Database {
+    private $host = "sql102.infinityfree.com";
+    private $db_name = "if0_39847940_Memestake";
+    private $username = "if0_39847940";
+    private $password = "227733"; // Add your MySQL password here
+    public $conn;
+
+    public function getConnection() {
+        $this->conn = null;
+        try {
+            $this->conn = new PDO(
+                "mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
+                $this->username, 
+                $this->password
+            );
+            $this->conn->exec("set names utf8");
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch(PDOException $exception) {
+            echo "Connection error: " . $exception->getMessage();
+        }
+        return $this->conn;
+    }
+}
+?>
